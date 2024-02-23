e6240512fed023950b4299fb951af01453727a17
Anton Karlsson <isanemka@gmail.com>
2024-02-24 00:11:17 +0100

    Add connection to db and fetch data in price list page

--- src/components/PriceListComponent.vue
+++ src/components/PriceListComponent.vue
@@ -7,30 +7,72 @@
       alt="Company logo"
     />
     <h1 class="text-h2 text-accent text-uppercase">Prislista</h1>
-    <p class="landing-body text-body1 text-accent q-pa-xl">
-      för smarta företagare
+    <p class="landing-body text-body1 text-accent q-pa-md">
+      Räkna snabbt ihop vad det kostar att få rena fönster
     </p>
-    <q-btn
-      class="q-pa-md text-black"
-      label="Se vad det kostar"
-      color="accent"
-    />
+    <q-table
+      :rows="articles"
+      row-key="id"
+      :columns="columns"
+      class="text-accent"
+    >
+      <template v-slot:body-cell-description="props">
+        <q-td :props="props">{{ props.row.description }}</q-td>
+      </template>
+      <template v-slot:body-cell-price="props">
+        <q-td :props="props">{{ props.row.price }}</q-td>
+      </template>
+      <template v-slot:body-cell-quantity="props">
+        <q-td :props="props">
+          <q-input v-model="props.row.quantity" type="number" min="1" dense />
+        </q-td>
+      </template>
+    </q-table>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { app } from '../firebase'
+
 export default defineComponent({
   name: 'PriceListComponent',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-    active: {
-      type: Boolean
-    }
-  }
+  data() {
+    return {
+      articles: [] as any[],
+      columns: [
+        {
+          name: 'description',
+          required: true,
+          label: 'Beskrivning',
+          align: 'left',
+          field: 'description'
+        },
+        {
+          name: 'price',
+          required: true,
+          label: 'Pris',
+          align: 'left',
+          field: 'price'
+        },
+        {
+          name: 'quantity',
+          required: true,
+          label: 'Antal',
+          align: 'left',
+          field: 'quantity' }
+      ]
+    };
+  },
+  async mounted() {
+    const db = getFirestore(app);
+    const articlesCollection = collection(db, 'articles');
+    const querySnapshot = await getDocs(articlesCollection);
+    querySnapshot.forEach((doc) => {
+      this.articles.push({ id: doc.id, ...doc.data() });
+    });
+  },
 });
 </script>
 
