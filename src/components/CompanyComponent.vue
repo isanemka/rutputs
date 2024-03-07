2f3ee001e313ed97775b3ecd922cd943b656d523
Anton Karlsson <isanemka@gmail.com>
2024-03-07 18:29:23 +0100

    Add content to company page

--- src/components/CompanyComponent.vue
+++ src/components/CompanyComponent.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="bg-primary text-center q-pa-md">
-
-    <h1 class="text-h2 text-accent text-uppercase">Rutputs på era villkor</h1>
-    <p class="landing-body text-body1 text-accent q-pa-xl">
-      för smarta företagare
-    </p>
+  <div class="row q-flex q-pa-md bg-primary justify-center items-center text-center">
+    <div class="col-lg-6 col-md-8 col-xs-12">
+      <h1 class="text-h2 text-accent text-uppercase">Rutputs på era villkor</h1>
+      <p class="landing-body text-body1 text-accent q-pa-xl">
+        Åt mina företagskunder utför jag alla typer av fönsterputsningar, helt
+        enligt företagets önskemål.
+        Med rena fönster på ditt företag så visar du att du tar både miljö och
+        arbetsmiljö på allvar för dina kunder och medarbetare.
+        Kanske vill ni ha rena skyltfönster, entréer, kontor, trapphus eller något annat?
+        Vi kan tillsammans skapa ett abonnemang skräddarsytt till just era behov
+        eller så kommer jag och putsar en gång, det är RUTPUTS på era villkor.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -12,15 +19,7 @@
 import { defineComponent } from 'vue';
 export default defineComponent({
   name: 'CompanyComponent',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-    active: {
-      type: Boolean
-    }
-  }
+  props: {}
 });
 </script>
 
