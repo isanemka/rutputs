a426aec06cdfe75b2ba6531e76e1b7a73bbb591b
Anton Karlsson <isanemka@gmail.com>
2024-04-25 21:39:15 +0200

    Checkout page foundation

--- src/components/CompanyComponent.vue
+++ src/components/CompanyComponent.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="row q-flex q-pa-md bg-primary justify-center items-center text-center">
-    <div class="col-lg-6 col-md-8 col-xs-12">
-      <h1 class="text-h2 text-accent text-uppercase">Rutputs på era villkor</h1>
-      <p class="landing-body text-body1 text-accent q-pa-xl">
-        Åt mina företagskunder utför jag alla typer av fönsterputsningar, helt
-        enligt företagets önskemål.
-        Med rena fönster på ditt företag så visar du att du tar både miljö och
-        arbetsmiljö på allvar för dina kunder och medarbetare.
-        Kanske vill ni ha rena skyltfönster, entréer, kontor, trapphus eller något annat?
-        Vi kan tillsammans skapa ett abonnemang skräddarsytt till just era behov
-        eller så kommer jag och putsar en gång, det är RUTPUTS på era villkor.
-      </p>
+  <q-page>
+    <div class="row q-flex q-pa-md bg-dark justify-center items-center text-center">
+      <div class="col-lg-6 col-md-8 col-xs-12">
+        <h1 class="text-h2 text-accent text-uppercase">Rutputs på era villkor</h1>
+        <p class="landing-body text-body1 text-accent q-pa-xl">
+          Åt mina företagskunder utför jag alla typer av fönsterputsningar, helt
+          enligt företagets önskemål.
+          Med rena fönster på ditt företag så visar du att du tar både miljö och
+          arbetsmiljö på allvar för dina kunder och medarbetare.
+          Kanske vill ni ha rena skyltfönster, entréer, kontor, trapphus eller något annat?
+          Vi kan tillsammans skapa ett abonnemang skräddarsytt till just era behov
+          eller så kommer jag och putsar en gång, det är RUTPUTS på era villkor.
+        </p>
+      </div>
     </div>
-  </div>
+  </q-page>
 </template>
 
 <script lang="ts">
--- src/components/LandingComponent.vue
+++ src/components/LandingComponent.vue
@@ -1,24 +1,26 @@
 <template>
-  <div class="bg-primary text-center q-pa-md">
-    <h1 class="text-h2 text-accent text-uppercase">
-      Din rutputsare i Avesta med omnejd
-    </h1>
-    <p class="landing-body text-body1 text-accent q-pa-xl">
-      Tack vare RUT-avdraget så kan Du få Dina rutor putsade för så lite som 350
-      kronor! Använd prislistan för att räkna ut Ditt pris! Många väljer att
-      endast få utsidan putsad och då lite oftare så ser man alltid bättre ut.
-      Fördelen är att man då inte ens behöver vara hemma vid själva putsningen.
-      Fråga efter putsabonnemang. Jag fakturerar genom Cool Company och tar
-      därför ut en liten avgift på 25 kr per faktura men i övrigt så finns det
-      inga dolda avgifter.
-    </p>
-    <q-btn
-      class="q-pa-md text-black"
-      label="Se vad det kostar"
-      color="accent"
-      @click="goToPriceList"
-    />
-  </div>
+  <q-page>
+    <div class="bg-dark text-center q-pa-md">
+      <h1 class="text-h2 text-accent text-uppercase">
+        Din rutputsare i Avesta med omnejd
+      </h1>
+      <p class="landing-body text-body1 text-accent q-pa-xl">
+        Tack vare RUT-avdraget så kan Du få Dina rutor putsade för så lite som 350
+        kronor! Använd prislistan för att räkna ut Ditt pris! Många väljer att
+        endast få utsidan putsad och då lite oftare så ser man alltid bättre ut.
+        Fördelen är att man då inte ens behöver vara hemma vid själva putsningen.
+        Fråga efter putsabonnemang. Jag fakturerar genom Cool Company och tar
+        därför ut en liten avgift på 25 kr per faktura men i övrigt så finns det
+        inga dolda avgifter.
+      </p>
+      <q-btn
+        class="q-pa-md text-black"
+        label="Se vad det kostar"
+        color="accent"
+        @click="goToPriceList"
+      />
+    </div>
+  </q-page>
 </template>
 
 <script lang="ts">
